e8bd122ed962d6356fb805c255f611b6275da959
7001362464 <57571284+7001362464@users.noreply.github.com>
2020-09-06 16:38:57 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,19 +1,17 @@
-from bmr import views
+from brm import views
 from django.conf.urls import url
 urlpatterns=[
-    url('viewbooks',views.viewbooks),
-    url('editbook',views.editbook),
-    url('searchbook', views.searchbook),
+    url('view_books',views.view_books),
+    url('edit_book',views.edit_book),
+    url('search_book', views.search_book),
     url('search', views.search),
     url('edit', views.edit),
-    url('deletebook', views.deletebook),
-    url('newbooks', views.NEWBOOK),
-    url('concatinate', views.add),
-    url('login', views.userlogin),
-    url('logout', views.userlogout),
+    url('del_book', views.del_book),
+    url('new_book', views.new_book),
 
-   url('download', views.download),
-
+    url('add1', views.add),
+    url('login',views.userlogin),
+url('logout',views.userlogout),
 
 
-]
+
