5b90c395213d2a0ad1785d4b0da40d06193dbc87
7001362464 <57571284+7001362464@users.noreply.github.com>
2020-09-06 16:39:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class boi(models.Model):
+class Book(models.Model):
     title=models.CharField(max_length=100)
     price=models.FloatField();
     author=models.CharField(max_length=100);
     publisher=models.CharField(max_length=100);
-    pdf=models.FileField(upload_to='documents/%Y/%m/%d')
+    #pdf=models.FileField(upload_to='documents/%Y/%m/%d')
 
